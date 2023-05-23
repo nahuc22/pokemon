d7d94f14a706f00b89db7bf4c832ca854cad9b33
Nahu Castilla <nahuelcastilla22@gmail.com>
2023-05-23 20:06:33 +0200

    models realizados

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,42 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    img: {
+      type: DataTypes.BLOB('long'),
+      allowNull: false
+    },
+    life: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    attack:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    speed:{
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    height:{
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    weight:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  },{timestamps: false});
 };
