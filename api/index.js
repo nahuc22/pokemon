b8ee40ed64ceded6843a75f62385c8526c290017
Nahuel Castilla <nahuelcastilla22@gmail.com>
2023-07-31 14:31:53 -0300

    deploy backend

--- api/index.js
+++ api/index.js
@@ -20,11 +20,10 @@
 require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log('listening at',PORT); // eslint-disable-line no-console
+  server.listen(3001, () => {
+    console.log('listening at',3001); // eslint-disable-line no-console
   });
 });
